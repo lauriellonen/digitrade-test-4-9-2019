a0f9dc44737caccf514b414ce17d389f38d3db47
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-09-17 11:59:43 +0300

    Example program

--- ExmapleAverage/ExmapleAverage/Program.cs
+++ ExmapleAverage/ExmapleAverage/Program.cs
@@ -6,7 +6,33 @@ namespace ExmapleAverage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int summa = 0;
+            int i = 1;
+            Console.Write("Ohjelma laskee annettujen lukujen keskiarvon." +
+                " Kuinka monta lukua haluat syöttää? ");
+            int lkm = int.Parse (Console.ReadLine());
+
+            while(i<=lkm)
+            {
+                int x = int.Parse(Console.ReadLine());
+                summa = summa + x;
+                i = i + 1;
+            }
+
+            if(lkm > 0)
+            {
+                double ka = summa / lkm;
+                Console.WriteLine("Lukujen keskiarvo on: " + ka);
+            }
+
+            else
+            {
+                Console.WriteLine("Lukuja ei ole syötetty! ");
+                    
+            }
+
+            Console.WriteLine(lkm);
+
         }
     }
 }
