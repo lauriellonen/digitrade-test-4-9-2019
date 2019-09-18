19ddf5d5fd211c55cce9177b421d0c3992b65dba
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-09-18 13:55:36 +0300

    This program will calculating

---
+++ Kertoma/Kertoma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+/// <summary>
+/// Tehdään ohjelma, joka tulostaa N:n arvon 
+/// Tehtävä 3. 
+/// Lauri Ellonen 
+/// </summary>
+
+namespace Kertoma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+}
