9627338f94ac448cbc65a29cf6d53d5ddea388f5
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-10-01 10:15:02 +0300

    IfTask3 ready but I may have to do some changes to code

--- IfTask1/IfTask3/Program.cs
+++ IfTask1/IfTask3/Program.cs
@@ -6,7 +6,35 @@ namespace IfTask3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma selvittää onko luku pariton vai parillinen " +
+                "ja onko luku negatiivinen vai positiivinen");
+            Console.Write("Syötä Luku ");
+            int userInputNumber = int.Parse(Console.ReadLine());
+
+            if (userInputNumber == 0)
+            {
+                Console.WriteLine($"Numero {userInputNumber} on nolla ja se on positiivinen!");
+            }
+            else
+            {
+                if (userInputNumber > 0)
+                {
+                    Console.WriteLine($"Numero {userInputNumber} on positiivinen!");
+                }
+                else
+                {
+                    Console.WriteLine($"Numero {userInputNumber} on negatiivinen");
+                }
+                if (userInputNumber % 2 == 0)
+                {
+                    Console.WriteLine($"ja {userInputNumber} on parillinen");
+                }
+                else
+                {
+                    Console.WriteLine($"ja {userInputNumber} on pariton");
+                }
+
+            }
         }
     }
 }
