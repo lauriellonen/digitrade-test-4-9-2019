305916e965fa94bc20ef202eae3332c7f8579bc1
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-10-01 11:11:36 +0300

    Created a discountProgramExample

---
+++ DiscountProgramExample/DiscountProgramExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DiscountProgramExample
+    /*
+     * This program is counting ticket discount for the users 
+     * Versio 1.0 made Lauri Ellonen 
+    */
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please...");
+        }
+    }
+}
