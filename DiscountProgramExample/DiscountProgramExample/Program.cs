d1d6423f4a2267ae1ddddbe64c4d61d8e9ac6610
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-10-03 10:00:42 +0300

    I started to code program

--- DiscountProgramExample/DiscountProgramExample/Program.cs
+++ DiscountProgramExample/DiscountProgramExample/Program.cs
@@ -13,10 +13,11 @@ namespace DiscountProgramExample
         {
 
             //Telling consumer to buy ticket
-            Console.WriteLine("Hello, please proceed to buy your ticket");
+            Console.WriteLine("Hello, please proceed to buy your ticket. " + "Please note: one discount per user " + 
+                "except if you are student and or a Mtk clubs member");
             //Informing them about prices
-            Console.WriteLine("Prices / Adult price 16 euros. / Child under 7 free. / Children over 7 years 8 euros. / " +
-                              "Elderly people 65 or over. / ");
+            Console.WriteLine("Prices / Adult price 16 euros. / Child under 7 free. / Children over 7 years 8 euros. / ");
+            Console.WriteLine("Elderly people 65 or over 8 euros. / ");
             Console.WriteLine("Student price is 15% discount remember to show your studentcard for the cashier.");
             Console.WriteLine(" / Service Man price. / Mtk club member price. ");
             
@@ -24,7 +25,18 @@ namespace DiscountProgramExample
             Console.Write("Please, input your age ");
             int userInputAge = int.Parse(Console.ReadLine());
 
+            if(userInputAge == 0)
+            {
+                Console.Write("Charge normal price 16 euros");
+            }
+            if (userInputAge == 7)
+            {
+                Console.Write("You'll have a free pass to our show.");
+            }
             
+
+
+
 
         }
     }
