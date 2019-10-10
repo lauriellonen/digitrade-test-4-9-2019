de4cf523dbaadc20511029b424f9b3826669bc08
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-10-10 11:07:12 +0300

    Made some major changes to code added correct prices and finished the program

--- DiscountProgramExample/DiscountProgramExample/Program.cs
+++ DiscountProgramExample/DiscountProgramExample/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
 namespace DiscountProgramExample
-    /*
-     * This program is counting ticket discount for the users 
-     * Versio 1.0 made by Lauri Ellonen 
-    */
+/*
+ * This program is counting ticket discount for the users 
+ * Versio 1.0 made by Lauri Ellonen 
+*/
 
 {
     class Program
@@ -12,30 +12,89 @@ namespace DiscountProgramExample
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello, please proceed to buy your ticket. " + "Please note: one discount per user " + 
-                "except if you are student and or a Mtk clubs member");
-            int ticketPrice = 16;
+            Console.WriteLine("Hello, please proceed to buy your ticket. " + "Please note: one discount per user " +
+                "except if you are student and or a Mtk clubmember");
 
-            //Customer will have to input their age
+            int ticketPrice = 25;
+            double studentDiscount = ticketPrice * 0.45;
+            double mtkClubDiscount = ticketPrice * 0.15;
+            double mtkStdentClubDiscount = ticketPrice * 0.15 + studentDiscount;
+            double serviceManDiscount = ticketPrice * 0.5;
+            /*
+             * Customer will have to input their age.
+             * And also the customers that are eligible for the discounted prices
+             * have been parametered. 
+            */
             Console.Write("Please, input your age ");
             int userInputAge = int.Parse(Console.ReadLine());
-            
+            String isStudent;
+            String isMtkClubMember;
+            String isServiceMan;
+            //String isSomeonePayingTicket;
+
             if (userInputAge <= 7)
             {
-                Console.WriteLine("Ticket is free");
+                Console.WriteLine("Your ticket price is free.");
             }
-            else if (userInputAge > 8 && userInputAge < 15)
+            else if (userInputAge >= 8 && userInputAge <= 15)
             {
-                Console.WriteLine($"Ticket price is {ticketPrice*0.5}");
+                Console.WriteLine($"Ticket price is {ticketPrice * 0.5} euros");
             }
-            if (userInputAge >= 16)
+            if (userInputAge >= 18 && userInputAge <= 27)
             {
-                Console.WriteLine($"No dicount. The ticket price is {ticketPrice}");
+                //Asking if the customer is in the service.
+                Console.Write("Are you in the service? y/n: ");
+                isServiceMan = Console.ReadLine();
+                if (isServiceMan == "y")
+                {
+                    Console.WriteLine($"Your ticket price is {ticketPrice - serviceManDiscount} euros");
+                }
+                else if (isServiceMan == "n")
+                {
+                    Console.WriteLine($"Your ticket price is {ticketPrice} euros");
+                }
             }
-            else if (userInputAge > 65 && userInputAge< 70 )
+            if (userInputAge >= 16 && userInputAge <= 59)
             {
-                Console.WriteLine($"Ticket price is {ticketPrice*0.5}");
+                //Asking a customer if they are student.
+                Console.Write("Are you a student? y/n: ");
+                isStudent = Console.ReadLine();
+                if (isStudent == "y")
+                {
+                    //Asking customer if they are a Mtk clubmember.
+                    Console.Write("Are you maybe a Mtk clubmemember? y/n: ");
+                    isMtkClubMember = Console.ReadLine();
+                    if (isMtkClubMember == "y")
+                    {
+                        Console.WriteLine($"Mtk ClubMember ticket price is {ticketPrice - mtkStdentClubDiscount} euros");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Your ticket price is {ticketPrice - studentDiscount} euros");
+                    }
+                }
+                else
+                {
+                    //Asking customer if they are a Mtk clubmember.
+                    Console.Write("Are you maybe a Mtk clubmemember? y/n: ");
+                    isMtkClubMember = Console.ReadLine();
+                    if (isMtkClubMember == "y")
+                    {
+                        Console.WriteLine($"Mtk ClubMember ticket price is {ticketPrice - mtkClubDiscount} euros");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Your ticket price is {ticketPrice} euros");
+                    }
+                }
+
+            }
+            if (userInputAge >= 60)
+            {
+                Console.WriteLine("Your ticket is free.");
             }
         }
     }
 }
+
+
