9cf2a5ecfae04a00869c8b7f0a154b76624efb45
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-10-15 10:32:49 +0300

    Added PasswordQueryExample to Ehto32Example

---
+++ Ehto32Example/PasswordQueryExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PasswordQueryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "qwerty1234";
+            int attemptCounter = 0;
+
+            Console.WriteLine("Input Password");
+            string userInpunt = Console.ReadLine();
+
+            while (userInpunt != password)
+            {
+                attemptCounter++;
+                if (attemptCounter >= 3)
+                {
+                    Console.WriteLine("Account is LOCKED!");
+                    return;
+                }
+
+                Console.WriteLine("Wrong Password! Please try again.");
+                userInpunt = Console.ReadLine();
+            }
+
+            Console.WriteLine("Password correct! ");
+
+        }
+    }
+}
