0ba257d0b3f4b6f5232bacc88029bec6fb204a7b
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-10-15 09:42:09 +0300

    Ehto32Program created

---
+++ Ehto32Example/Ehto32Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ehto32Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program will calculate N number multiply");
+            Console.Write("Input number ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= userInput; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
