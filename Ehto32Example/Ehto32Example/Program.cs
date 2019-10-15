68d2bb77d0198819261ad8bd5337be80cabd02f6
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-10-15 10:07:48 +0300

    While, DoWhile and For term Example

--- Ehto32Example/Ehto32Example/Program.cs
+++ Ehto32Example/Ehto32Example/Program.cs
@@ -10,10 +10,39 @@ namespace Ehto32Example
             Console.Write("Input number ");
             int userInput = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i <= userInput; i++)
+            Console.WriteLine("For ehto ---------------------------------");
+            for (int i = 0; i < userInput; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(i+1);
             }
+
+            Console.WriteLine("WHILE ------------------------------------");
+            int j = 0;                          //Calculate parameter
+            while(j < userInput)                //Loop is done until it is False 
+            {
+                Console.WriteLine(j + 1);
+                j = j + 1;                      //Increase the counter
+                                                //or j++;
+            }
+
+            Console.WriteLine("WHILE none stop loop ----------------------");
+            int k = 0;
+            while (true)                        //none stop loop 
+            {
+                k++;
+                Console.WriteLine(j);           
+                if (k >= userInput)
+                    break;
+            }
+
+            Console.WriteLine("Do-WHILE ----------------------------------");
+            int n = 0;
+            do
+            {
+                n++;
+                Console.WriteLine(n);
+
+            } while (n < userInput);
         }
     }
 }
