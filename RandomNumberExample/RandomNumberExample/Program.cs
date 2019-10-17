7314480f178a751cb0c44c3cb6515ac2bd94376a
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-10-17 14:29:25 +0300

    Versio 0.1.0

---
+++ RandomNumberExample/RandomNumberExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+/// <summary>
+/// This is my random number program example
+/// made by Lauri Ellonen 
+/// </summary>
+namespace RandomNumberExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input... ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            new Random();
+        }
+    }
+}
