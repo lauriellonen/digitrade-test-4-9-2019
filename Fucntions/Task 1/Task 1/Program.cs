bc9d061d9b26279f014790bba802dce8314c354e
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-10-28 12:50:33 +0200

    Created Functions Task 1 - added comments - added some code - made program ready to use

---
+++ Fucntions/Task 1/Task 1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        /// <summary>
+        /// Main Program. 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The program will print the number of stars according to the number user is input");
+            Console.Write("Input full number: ");
+            int userInputNumber = int.Parse(Console.ReadLine());
+            if (IsValidNumber(userInputNumber))
+            {
+                string strStars = StarAmountGenerator(userInputNumber);
+                Console.WriteLine(strStars);
+                //Console.WriteLine(StarAmountGenerator(userInput))
+            }
+            else
+            {
+                Console.WriteLine($"{userInputNumber} Is invalid! ");
+            }
+
+        }
+        
+        static bool IsValidNumber(int userInputNumber)
+        {
+            return userInputNumber > 0;
+
+            /*if (userInputNumber > 0)
+            {
+                return true;
+            }
+            return false;
+            */
+        }
+
+        /// <summary>
+        /// The Program will generate the parameter amount of star. 
+        /// </summary>
+        /// <param name="StarsCounter"></param>
+        /// <returns></returns>
+
+        static string StarAmountGenerator(int StarsCounter)
+        {
+            string stars = "";
+            for (int i = 0; i < StarsCounter; i++)
+            {
+                stars = stars + "*";    //old way. 
+                /*stars += "*"; */      //Microsoft's new recomended way to do this thing. 
+            }
+            return stars;
+        }
+    }
+}
