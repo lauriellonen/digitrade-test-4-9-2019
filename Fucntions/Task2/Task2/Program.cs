f8a79a97bf05464e59688a5ae946a59450a7b394
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-10-30 11:47:20 +0200

    Added comments to Task2 and also added versio number.

--- Fucntions/Task2/Task2/Program.cs
+++ Fucntions/Task2/Task2/Program.cs
@@ -2,11 +2,18 @@
 
 namespace Task2
 {
+    /// <summary>
+    /// Made by Lauri Ellonen
+    /// Program versio 0.1.0 
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            /* User will input two number between zero and ten and then the program will 
+               answer which of the input number is smaller.
+             */
+            Console.WriteLine("Please input two numbers between zero and ten.");
         }
     }
 }
