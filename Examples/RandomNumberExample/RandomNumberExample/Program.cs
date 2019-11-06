306b34e33cf9015d519c7ef8a101d27decb700ee
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-11-06 10:01:15 +0200

    Fixed code. so that it will now show numbers in the row format.

--- Examples/RandomNumberExample/RandomNumberExample/Program.cs
+++ Examples/RandomNumberExample/RandomNumberExample/Program.cs
@@ -11,28 +11,27 @@ namespace RandomNumberExample
         static void Main(string[] args)
         {
 
-            Console.Write("Hello world: ");
             //Random number generation
             Random rnd = new Random();
             #region
             //Loop that prints the rows 
             for (int r = 0; r < 4; r++)
             {
-                Console.WriteLine($"Rivi{r+1}: ");
+                Console.Write($"Rivi{r+1}: ");
 
                 for (int i = 0; i < 5; i++)
                 {
                     if (i < 4)
                     {
-                        Console.WriteLine($"{rnd.Next(0, 51)}, ");
+                        Console.Write($"{rnd.Next(0, 51)}, ");
                     }
 
                     else
                     {
-                        Console.Write(rnd.Next(0, 51));
+                        Console.WriteLine(rnd.Next(0, 51));
                         
                     }
-                    Console.WriteLine();
+                    
                 }
             }
             #endregion
