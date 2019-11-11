2cff9fd3ea4b40909bdacc887391421c8046d563
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-11-11 18:42:53 +0200

    IfTask14 created

---
+++ IfTasks/IfTask14/IfTask14/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IfTask14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World! ");
+            int userInput = int.Parse(Console.ReadLine());
+        }
+    }
+}
