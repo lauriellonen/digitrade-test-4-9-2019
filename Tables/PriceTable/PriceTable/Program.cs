783509ae385617e0e6fd3c816d4b3bac9b0dd82e
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-11-15 09:17:21 +0200

    I deleted the older Array project and started a new project PriceTable

---
+++ Tables/PriceTable/PriceTable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PriceTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
