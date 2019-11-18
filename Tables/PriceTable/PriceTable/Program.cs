85359dbebedf90a59b676567feed667dda7264b1
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-11-18 12:07:42 +0200

    Added more code

--- Tables/PriceTable/PriceTable/Program.cs
+++ Tables/PriceTable/PriceTable/Program.cs
@@ -2,14 +2,16 @@
 
 namespace PriceTable
 {
+
+    /*
+     * In this Table I will created numbers with decimal.  
+     */
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("PriceTable");
-            /*
-             * In this Table I will created numbers with decimal.  
-             */
+
         }
     }
 }
