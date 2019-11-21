b19325c122d2f8dcd8be8d4458bb9a21999ffcc3
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-11-21 09:51:37 +0200

    Started new project

---
+++ Fucntions/Task3/Task3_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Function task 3.1");
+            string userInput = Console.ReadLine();
+
+
+        }
+    }
+}
