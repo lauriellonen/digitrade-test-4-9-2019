972287530673f877fa1038dfffafd384c7ef9702
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-11-26 08:37:29 +0200

    Added more code and comments and also finnished this program note: I used a and b instead of x and y.

--- Fucntions/Task2/Task2/Program.cs
+++ Fucntions/Task2/Task2/Program.cs
@@ -13,7 +13,37 @@ namespace Task2
             /* User will input two number between zero and ten and then the program will 
                answer which of the input number is smaller.
              */
-            Console.WriteLine("Please input two numbers between zero and ten.");
+            Console.WriteLine("User will input two numbers and this program will tell which number is smaller.");
+            Console.Write("please input first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write( "please input second number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Luku {GetMinNumber(a, b)} on pienempi kuin {GetMaxNumber(a, b)}");
+        }
+        static int GetMinNumber(int firstNumber, int secondNumber)
+        {
+            if (firstNumber < secondNumber) //return firstNumber < secondNumber? firstNumber: secondNumber;
+            {
+                return firstNumber;
+            }
+            else
+            {
+                return secondNumber;
+            }
+        }
+
+
+        static int GetMaxNumber(int firstNumber, int secondNumber)
+        {
+            if (firstNumber > secondNumber)
+            {
+                return firstNumber;
+            }
+            else
+            {
+                return secondNumber;
+            }
         }
     }
 }
