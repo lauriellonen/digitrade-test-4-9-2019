3bba6bfb4fcd43d2fdf41f456463fd3070c80b57
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-11-26 08:57:14 +0200

    Started Functions Task 4.

---
+++ Fucntions/Functions Task4/Functions Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Functions_Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma palauttaa syötetyistä luvuista suurimman luvun");
+            MaxNumberCalculator();
+        }
+        static string MaxNumberCalculator()
+        {
+            Console.WriteLine("Syötä 10 kokonaislukua");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i + 1}.");
+            }
+            return "";
+        }
+    }
+}
