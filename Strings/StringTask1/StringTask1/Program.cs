6152d44f93b4d51801d1e2da055e7ee9e0f687a4
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-11-26 10:32:49 +0200

    Prints string values one by one and added comments.

--- Strings/StringTask1/StringTask1/Program.cs
+++ Strings/StringTask1/StringTask1/Program.cs
@@ -6,7 +6,15 @@ namespace StringTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Merkkijono käsittely esimerkki");
+            string message = "Hello World";
+            //Tulostetaan messages -muuttujen merkit yksitellen näytölle.
+            Console.WriteLine($"{message} on {message.Length} merkkiä.");
+
+            for (int i =0;i<message.Length; i++)
+            {
+                Console.WriteLine(message[i]);
+            }
         }
     }
 }
