46f5ea27758d8d8144baab4ab326ba1c5697fbb1
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-11-26 09:21:35 +0200

    resultMessage Done and printed in main program

--- Fucntions/Functions Task4/Functions Task4/Program.cs
+++ Fucntions/Functions Task4/Functions Task4/Program.cs
@@ -7,16 +7,37 @@ namespace Functions_Task4
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma palauttaa syötetyistä luvuista suurimman luvun");
-            MaxNumberCalculator();
+            string resultMessage = MaxNumberCalculator();
+            Console.WriteLine($"Syötit seuraavat luvut:\n {resultMessage}");
         }
         static string MaxNumberCalculator()
         {
+            int maxNumber=0;
+            int sequenceOfNumber;
+            string message = string.Empty;
             Console.WriteLine("Syötä 10 kokonaislukua");
             for (int i = 0; i < 10; i++)
             {
                 Console.Write($"{i + 1}.");
+                string userInput = Console.ReadLine();
+                bool isnumber = int.TryParse(userInput, out int userValue);
+
+                if (isnumber)
+                {
+                    message = $"{message}, {userValue}"; 
+                    if (userValue > maxNumber)
+                    {
+                        maxNumber = userValue;
+                        sequenceOfNumber = i + 1;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Väärä syöte, syötä luku:");
+                    i--;        
+                }
             }
-            return "";
+            return message;
         }
     }
 }
