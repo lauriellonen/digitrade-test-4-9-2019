50d495e3f9daa2e2dca8f2746430efd283ff0b90
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-12-12 13:38:13 +0200

    HETUprogram - added Input Parser - added isValidID

--- checkmark/HETUprogram/HETUprogram/Program.cs
+++ checkmark/HETUprogram/HETUprogram/Program.cs
@@ -6,7 +6,26 @@ namespace HETUprogram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tarkastaa Hetun oikeudellisuuden.");
+            string userInput = "061096-083C";
+            int idNumber = InputParser(userInput);
+            isValidID(idNumber);
+
+        }
+        static int InputParser(string stringParser)
+        {
+            string removed = stringParser.Remove(10, 1);
+            removed = removed.Remove(6, 1);
+            //Luetaan 
+            string result = stringParser.Substring(0, 6);
+            return int.Parse(removed);
+        }
+        static bool isValidID(int idNumber)
+        {
+            string chkMark = "0123456789ABCDEFHJKLPRSTUVWXY";
+            int mChecker = idNumber % 31;
+            Console.WriteLine(chkMark[mChecker]);
+            return true;
         }
     }
 }
