9b4612e69943941996e59e505767536b0f8c96f0
lauriellonen <lauri.ellonen@student.saimia.fi>
2019-12-12 12:48:30 +0200

    Started HETUprogram project.

---
+++ checkmark/HETUprogram/HETUprogram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HETUprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
